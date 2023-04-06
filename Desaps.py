467ca6d33221526be8fe8aa1c0fe96a060564e92
Daniel Lucas <danieldodesign2005@gmail.com>
2023-04-06 19:09:03 -0300

    Crawler

--- Desaps.py
+++ Desaps.py
@@ -6,12 +6,15 @@ content = response.content#o ponto content serve para ler o html do site acima
 
 site = bs(content, 'html.parser')#o ponto parse converte em html
 #print(site.prettify()) #o ponto prettify serve para estruturar o código igual ao html real do site
-ver = site.findAll('i')#attrs serve para procurar um tributo no html
+ver = site.findAll('i')#attrs serve para procurar um atributo no html
 #print(ver)
 
+
 for v in ver:
-    nome = v.find("span")
+    nome = v.findAll("span")
     print(nome)
-    
+    city = v.find('span', attrs={'id' : 'ctl2'})
+    print("¨¨¨¨¨¨")
+    print(city)
 
 #print(type(ver))
