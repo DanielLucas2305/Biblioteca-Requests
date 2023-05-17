4eda9eebdc305da50a863448f935b40b71cbd04e
Daniel Lucas <danieldodesign2005@gmail.com>
2023-05-17 20:16:04 -0300

    AlteracaoFor

--- desaparecidosSelenium.py
+++ desaparecidosSelenium.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from time import sleep
 import csv
-import sqlite3
 navegador = webdriver.Chrome()
 
 all = [] #lista vazia
@@ -23,13 +22,23 @@ def searchs():
 
 #Inicio da busca no site
 navegador.get("http://200.144.31.45/desaparecidos/default.aspx?")
-pags = navegador.find_element('xpath', '//*[@id="LkBtnPesquisar"]').click()
+sleep(4)
+pags = navegador.find_element('xpath', '//*[@id="LkBtnLimpar"]').click()
 sleep(2)
 segpag = navegador.find_element('xpath', '//*[@id="link2"]').click()
 sleep(3)
 
-#Loop para busca em todas as páginas de mulheres
-for a in range(1, 139):
+#Loop para busca em todas as páginas
+for a in range(1, 3):
+    segpag = navegador.find_element('xpath', f'//*[@id="link{a}"]').click()
+    searchs()
+    print("-#-"*7,f"Página([{a}]) Abaixo",'-*-'*7)
+    print(all)
+    print("-#-"*7,f"Página([{a}]) Acima",'-*-'*7)
+
+sleep(3)
+
+for a in range(3, 139):
     segpag = navegador.find_element('xpath', f'//*[@id="link{a}"]').click()
     searchs()
     print("-#-"*7,f"Página([{a}]) Abaixo",'-*-'*7)
@@ -46,6 +55,7 @@ csvfile.close()
 
 
 '''
+# outras anotações
 #homens
 
 //*[@id="link1"]
