9f476dd33cc828cdd64704f96228e19548a0ee04
Daniel <danieldodesign2005@gmail.com>
2024-05-07 09:41:45 -0300

    feat: teste comparacao de precos

---
+++ MercadoLivre.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+navegador = webdriver.Chrome()
+navegador.get("https://lista.mercadolivre.com.br/contrabaixo")
+#navegador.get("https://lista.mercadolivre.com.br/contrabaixo").sendkeys("texto q quer") comandopara escrever em algum lugas
+#navegador.find_element("Colocar o xpath do lugar q quer acahr") + .sendkeys("texto q quer") comando para escrever em algum lugas
